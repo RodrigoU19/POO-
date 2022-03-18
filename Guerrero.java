256236149c59a97d7215b97409955e25259dcd7a
Rodrigo Neftali Aguilar Uribe <98993165+RodrigoU19@users.noreply.github.com>
2022-03-18 16:08:10 -0600

    Create Guerrero.java

---
+++ Guerrero.java
@@ -0,0 +1,29 @@
+package pvp;
+
+import java.util.Random;
+
+public class Guerrero extends Personaje{
+    
+    public Guerrero(){
+    
+    super("Guerrero",200,35,25,0.6," combo de golpes de furia mortal ");
+    
+}
+    
+    public void aumentoDefensa(){
+        
+        Random r = new Random();
+        int a = r.nextInt(200);
+        if(a>100&& a>135)
+        {
+            int defensa = this.getDefensa();
+            defensa+=1;
+            this.setDefensa(defensa);
+            double ta = this.getTa();
+            ta=0.20;
+            this.setTa(ta);
+            System.out.println("Mis golpes han mejorado... ya no me asustas mas");
+
+        }
+        
+    }
